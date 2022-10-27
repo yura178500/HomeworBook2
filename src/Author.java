75bf269a16feeda324593e0b26c9f7ee3292a32e
yura178500 <yura178500@yandex.ru>
2022-10-27 18:52:51 +0300

    Задача Исправил

--- src/Author.java
+++ src/Author.java
@@ -1,20 +1,36 @@
 public class Author {
-    private String author;
+    private String lastName;
+    private String firstName;
+    private String patronymic;
 
-    public Author(String author) {
-        this.author = author;
+    public Author(String firstName, String lastName, String patronymic) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+
     }
 
-    public String getAuthor() {
-        return this.author;
+    public String getFirstName() {
+        return this.firstName;
+
+    }
+
+    public String getLastName() {
+        return this.lastName;
+
+    }
+
+    public String getPatronymic() {
+        return this.patronymic;
 
     }
 
     public String toString() {
-        return "  ФИО   " + this.author;
+        return "  Имя" + this.lastName + "  Фамилия   " + this.firstName + "  Отчество   " + this.patronymic;
     }
+
     public boolean equals(Author book1) {
-        return this.author == book1.author;
+        return lastName == book1.lastName && firstName == book1.firstName && patronymic == book1.patronymic;
     }
 }
 
--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,18 @@
 public class Book {
     private String name;
+    private String author;
     private int age;
 
-    public Book(String name, int age) {
+
+    public Book(String name, String author, int age) {
         this.name = name;
+        this.author = author;
         this.age = age;
+
+    }
+
+    public String getAuthor() {
+        return this.author;
     }
 
     public String getName() {
@@ -16,18 +24,20 @@ public class Book {
     }
 
     public String toString() {
-        return "  Названия  " + name + "   дата выпуска  " + age;
+        return "  Названия  " + name + "  ФИО  " + author + "   дата выпуска  " + age;
     }
 
     public void setAge(int age) {
         this.age = age;
     }
 
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book bookSet1 = (Book) o;
-        return age == bookSet1.age && name.equals(bookSet1.name);
+
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book bookSet1 = (Book) other;
+        return age == bookSet1.age && name.equals(bookSet1.name) && author.equals(bookSet1.author);
     }
 
     private int result;
@@ -36,6 +46,7 @@ public class Book {
         String result = String.valueOf(name == null ? 0 : name.hashCode());
         result = result + name;
         result = result + age;
+        result = result + author;
         return this.result;
     }
 }
